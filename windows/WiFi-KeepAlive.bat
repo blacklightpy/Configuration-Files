49032eeb7f6874929a576fa4efd28cc9ef2d99f2
Jyothish Kumar M S <jeevanms15@gmail.com>
2022-02-17 12:25:08 +0530

    Create WiFi-KeepAlive.bat

---
+++ windows/WiFi-KeepAlive.bat
@@ -0,0 +1,11 @@
+REM Restart Wireless Connection
+REM Don’t restart if ping works.
+
+call config.bat wifi_settings
+
+ping -n 1 google.com
+if %errorlevel% EQU 0 goto end
+netsh wlan disconnect interface="%INTERAFCE%"
+TIMEOUT 5
+netsh wlan connect ssid="%SSID%" name="%PROFILE_NAME%" interface="%INTERFACE%"
+:end
